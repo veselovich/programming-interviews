39867b9296b9341889ef8c6db7b9d9cc39e79771
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-08 21:02:06 -0500

    initial commit

--- brute.py
+++ brute.py
@@ -3,23 +3,30 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def swap_bits(x,i,j):
     """
-    Name
-    Example
+    Swap bits it binary word
+    Given indecies i,j = 1, 2
+    [1,1,0,0] -> [1,0,1,0]
+    12 -> 10
     
-    :type x: int
+    :type x,i,j: int 
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    # extract i and j
+    if (x >> i) & 1 != (x >> j) & 1:
+        # create bit mask 
+        bit_mask = (1 << i) | (1 << j)
+        # flip with XOR
+        x ^= bit_mask
+    return x       
 
 
 def main():
     start_time = time.time()
 
     #test case
-    #TODO
+    print(swap_bits(12, 1, 2))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
