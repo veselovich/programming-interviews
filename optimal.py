f4a5bd1ef583f947b38776d68524877f2f1b9eda
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-10 17:57:10 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,26 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def reverse(x):
     """
-    Name
-    Example
+    Reverese input without converting it to the string
+    123 -> 321
     
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    result, x_remaining = 0, abs(x)
+    while x_remaining:
+        result = result * 10 + x_remaining % 10
+        x_remaining //= 10
+    return -result if x < 0 else result
 
 
 def main():
     start_time = time.time()
 
     #test case
-    #TODO
+    print(reverse(123))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
