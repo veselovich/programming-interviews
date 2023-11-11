c544885cfa2d50bf7b70c5f59eb1d7e7c93e8036
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-10 19:08:55 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,34 @@
 import time
 from memory_profiler import profile
 
+from random import randint as zero_one_random
 
 @profile
-def func_name(x):
+def uniform_random(lower_bound, upper_bound):
     """
-    Name
-    Example
+    Generate random number within bounds
+    1, 10 -> 7
     
-    :type x: int
+    :type lower_bound, upper_bound: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    number_of_outcomes = upper_bound - lower_bound + 1
+    while True:
+        result, i = 0, 0
+        while (1 << i) < number_of_outcomes:
+            # zero_one_random () is the provided random number generator.
+            result = (result << 1) | zero_one_random(0, 1)
+            i += 1
+        if result < number_of_outcomes:
+            break
+    return result + lower_bound
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(uniform_random(1, 10))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
