e3a4a23d09b231a9e42a3647db6aef3d488ea166
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-20 17:13:56 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,28 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def buy_and_sell_stock_once(prices):
     """
-    Name
-    Example
+    Given array representing a stock price. Maximize profit from one trade
+    Example: [310,315,275,295,260,270,290,230,255,250] -> 30
     
-    :type x: int
+    :type prices: list
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+    return max_profit
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    prices = [310,315,275,295,260,270,290,230,255,250]
+    print(buy_and_sell_stock_once(prices))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
