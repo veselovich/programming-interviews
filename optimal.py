68330dbf9aa90b1742b0e7bab3852a1bcd3d29cb
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-20 15:44:55 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,27 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def can_reach_end(A):
     """
-    Name
-    Example
+    Go through array. Each value in aggary is possible range to move forward
     
-    :type x: int
+    :type A: list
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    furthest_reach_so_far, last_index = 0, len(A) - 1
+    i = 0
+    while i <= furthest_reach_so_far and furthest_reach_so_far < last_index:
+        furthest_reach_so_far = max(furthest_reach_so_far, A[i] + i)
+        i += 1
+    return furthest_reach_so_far >= last_index
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    A = [3,3,1,0,2,0,1]
+    print(can_reach_end(A))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
