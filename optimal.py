7387b699f99db3f58b7bb200aa744caa69429e76
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-20 16:36:13 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,31 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def delete_duplicates(A) :
     """
-    Name
-    Example
+    Delete all duplicates from sorted array in place
+    Example: [2,3,5,5,7,11,11,11,13] -> [2,3,5,7,11,13,any,any,any]
     
-    :type x: int
+    :type A: list
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    if not A:
+        return 0
+    write_index = 1
+    for i in range (1, len(A)):
+        if A[write_index - 1] != A[i]:
+            A[write_index] = A[i]
+            write_index += 1
+    return write_index
 
 
 def main():
     start_time = time.time()
-
+    
     #test case
-    print(func_name(0))
+    A = [2,3,5,5,7,11,11,11,13]
+    print(delete_duplicates(A))
+    print(A)
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
