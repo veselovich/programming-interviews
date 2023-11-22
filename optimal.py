0d0dc696e5396d5fd24de1f7337920d6203404f6
veselovich <129292923+veselovich@users.noreply.github.com>
2023-11-21 21:25:12 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,25 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def rearrange(A):
     """
-    Name
-    Example
+    Rearrange array so values are alternate: one is more than next and next is vice versa
+    Example: A[1] <= A[2] >= A[3] <= A[4] ...
     
-    :type x: int
-    :rtype: int
+    :type A: list
+    :rtype: None
     """
-    #TODO
-    raise NotImplemented
+    for i in range(len(A)):
+        A[i:i+2] = sorted(A[i:i+2], reverse=i % 2)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    A = [234,32,13,342,456,32,564,45]
+    rearrange(A)
+    print(A)
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
