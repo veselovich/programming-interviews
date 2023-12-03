9f0674de33f4de9723c4105be48c7ba8ec988dcc
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-03 13:31:46 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,36 @@
 import time
+import random
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def random_subset(n, k):
     """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
+    Compute a random subset given length
+    Example: n = 4, k = 2
+    [0,1,2,3] -> [1,2]
+
+    :type n, k: int
+    :rtype: list
     """
-    #TODO
-    raise NotImplemented
+    changed_elements = {}
+    for i in range(k):
+        # Generate a random index between i and n - 1, inclusive.
+        rand_idx = random.randrange(i, n)
+        rand_idx_mapped = changed_elements.get(rand_idx, rand_idx)
+        i_mapped = changed_elements.get(i, i)
+        changed_elements[rand_idx] = i_mapped
+        changed_elements[i] = rand_idx_mapped
+    return [changed_elements[i] for i in range(k)]
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    n = 6
+    k = 2
+    print(random_subset(n, k))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
