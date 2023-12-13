8f4acf952c46d577cef6a6deebfbb884b310a9e2
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-13 16:16:49 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,45 @@
+import functools
+import string
 import time
-from memory_profiler import profile
 
 
-@profile
-def func_name(x):
+def int_to_string(x):
     """
-    Name
-    Example
-    
+    Converts integer to string
+        
     :type x: int
+    :rtype: str
+    """
+    is_negative = False
+    if x < 0:
+        x, is_negative = -x, True
+    s = []
+    while True:
+        s.append(chr(ord('0') + x % 10))
+        x //= 10
+        if x == 0:
+            break
+    # Adds the negative sign back if is_negative
+    return ('-' if is_negative else '') + ''.join(reversed(s))
+
+def string_to_int (s):
+    """
+    Converts string to integer
+        
+    :type s: str
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    return functools.reduce(
+        lambda running_sum, c: running_sum * 10 + string.digits.index(c),
+        s[s[0] == '-':], 0) * (-1 if s[0] == '-' else 1)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(int_to_string(-123))
+    print(string_to_int("-456"))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
