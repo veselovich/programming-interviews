38d8ed677f8192f132367a4a19a241aca3d926c8
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-13 19:11:58 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,46 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def replace_and_remove(size, s):
     """
-    Name
+    Removes all 'b' from array and replace 'a' to 'd', 'd'
     Example
     
-    :type x: int
+    :type size: int
+    :type s: list
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    # Forward iteration: remove 'b's and count the number of 'a's.
+
+    write_idx, a_count = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+    # Backward iteration: replace 'a's with 'dd's starting from the end.
+    cur_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+    while cur_idx >= 0:
+        if s[cur_idx] == 'a':
+            s[write_idx - 1: write_idx + 1] = 'dd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[cur_idx]
+            write_idx -= 1
+        cur_idx -= 1
+    return final_size
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    arr = ['a','c','d','b','b','c','a']
+    print(f := replace_and_remove(6, arr))
+    print(arr[:f])
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
