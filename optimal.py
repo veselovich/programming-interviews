94355e0680f349ebe66d34b759ddf175e6d087a5
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-14 18:10:56 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,41 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def reverse_words(s):
     """
-    Name
-    Example
+    Reverse words in string
     
-    :type x: int
-    :rtype: int
+    :type s: bytearray
+    :rtype: None
     """
-    #TODO
-    raise NotImplemented
+    # Assume s is a string encoded as bytearray.
+    # First, reverse the whole string.
+    s.reverse()
+
+    def reverse_range(s, start, end):
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start, end = start + 1, end - 1
+    start = 0
+    while True:
+        end = s.find(b' ', start)
+        if end < 0:
+            break
+
+        # Reverses each word in the string.
+        reverse_range(s, start, end - 1)
+        start = end + 1
+    # Reverses the last word.
+    reverse_range (s, start, len(s) - 1)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    s = bytearray("Alice likes Bob", 'utf-8')
+    reverse_words(s)
+    print(s)
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
