027cfb2953187d7a03a480eaf1a984aa3904eee0
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-14 16:56:58 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,32 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def is_palindrome(s):
     """
-    Name
-    Example
+    Check if string is palindrome
     
-    :type x: int
-    :rtype: int
+    :type s: str
+    :rtype: bool
     """
-    #TODO
-    raise NotImplemented
+    # i moves forward, and j moves backward.
+    i, j = 0, len(s) - 1
+    while i < j:
+        # i and j both skip non-alphanumeric characters.
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i, j= i + 1, j - 1
+    return True
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(is_palindrome("A man, a plan, a canal, Panama"))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
