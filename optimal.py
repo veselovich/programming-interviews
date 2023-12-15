7701636ac16fbc0733ebdc8e31725e4dcd4e531e
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-14 20:37:54 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,46 @@
+import itertools
 import time
-from memory_profiler import profile
 
 
-@profile
-def func_name(x):
+def look_and_say(n):
     """
-    Name
-    Example
+    Genereate sequence of digits, pronouncing previous sequence
     
-    :type x: int
-    :rtype: int
+    :type n: int
+    :rtype: List[int]
     """
-    #TODO
-    raise NotImplemented
+    def next_number(s):
+        result, i = [], 0
+        while i < len(s):
+            count = 1
+            while i + 1 < len(s) and s[i] == s[i + 1]:
+                i += 1
+                count += 1
+            result.append(str(count) + s[i])
+            i += 1
+        return ''.join(result)
+
+    s = '1'
+    for _ in range(1, n):
+        s = next_number(s)
+    return s
+
+# Pythonic solution uses the power of itertools.groupby().
+def look_and_say_pythonic(n):
+    s = '1'
+    for _ in range(n - 1):
+        s = ''.join(
+            str(len(list(group))) + key for key, group in itertools.groupby(s))
+    return s
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+
+    print(look_and_say(8))
+    print(look_and_say_pythonic(8))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
