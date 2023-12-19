39f38b656050e39d1d148613eb6cb61648900a24
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-18 20:41:17 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,29 @@
+import functools
 import time
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def roman_to_integer(s):
     """
-    Name
-    Example
+    Converts valid roman integer to decimal
+    "LIX" -> 59
     
-    :type x: int
+    :type s: str
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    T = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return functools.reduce(
+        lambda val, i: val + (-T[s[i]] if T[s[i]] < T[s[i + 1]] else T[s[i]]),
+        reversed(range(len(s) - 1)), T[s[-1]])
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    roman = "LIX"
+    print(roman_to_integer(roman))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
