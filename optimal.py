d26e9443e59d64be46f080bc03f5146568f00e1b
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-19 13:18:17 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,36 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def snake_string(s) :
     """
-    Name
-    Example
+    Write given string in snake order
     
-    :type x: int
-    :rtype: int
+    :type s: str
+    :rtype: str
     """
-    #TODO
-    raise NotImplemented
+    result = []
+    # Outputs the first row, i.e., s[1], s[5], s[9],
+    for i in range(1, len(s), 4):
+        result.append(s[i])
+    # Outputs the second row, i.e., s[0], s[2], s[4],
+    for i in range(0, len(s), 2):
+        result.append(s[i])
+    # Outputs the third row, i.e., s[3], s[7], s[11],
+    for i in range(3, len(s), 4):
+        result.append(s[i])
+    return ''.join(result)
+
+# Python solution uses slicing with right steps.
+def snake_string_pythonic(s):
+    return s[1::4] + s[::2] + s[3::4]
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(snake_string("Hello World"))
+    print(snake_string_pythonic("Hello World"))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
