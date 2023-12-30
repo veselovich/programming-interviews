666c4bbfa723b7bf8bda7cd675465fcc70156d12
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-30 18:19:14 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,37 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
+def merge_two_sorted_lists(L1, L2):
     """
-    Name
-    Example
+    Merges two sorted linked list into a one
     
-    :type x: int
-    :rtype: int
+    :type L1: ListNode
+    :type L2: ListNode
+    :rtype: ListNode
     """
-    #TODO
-    raise NotImplemented
+    # Creates a placeholder for the result.
+    dummy_head = tail = ListNode()
+    
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+        tail = tail.next
+    # Appends the remaining nodes of L1 or L2
+    tail.next = L1 or L2
+    return dummy_head.next
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print_list(merge_two_sorted_lists(L1, L2))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
