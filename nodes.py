b070647d51aaf62716d4d9916f89a007b031be3f
veselovich <129292923+veselovich@users.noreply.github.com>
2023-12-30 20:32:54 -0500

    initial commit

---
+++ nodes.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, data=0, next_node=None):
+        self.data = data
+        self.next = next_node
+    
+    def __repr__(self) -> str:
+        return f"Node {self.data}"
+
+
+def print_list(head):
+    while head:
+        print(head.data, end=" -> ")
+        head = head.next
+    print("None")
+
+
+L1 = ListNode(1)
+L1.next = ListNode(3)
+L1.next.next = ListNode(5)
+L1.next.next.next = ListNode(7)
+L1.next.next.next.next = ListNode(9)
+L1.next.next.next.next.next = ListNode(11)
+L1.next.next.next.next.next.next = ListNode(13)
+
+L2 = ListNode(2)
+L2.next = ListNode(4)
+L2.next.next = ListNode(6)
+L2.next.next.next = ListNode(8)
+L2.next.next.next.next = L1.next.next.next.next
--- optimal.py
+++ optimal.py
@@ -1,25 +1,41 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
+def overlapping_no_cycle_lists(L1, L2):
     """
-    Name
-    Example
+    Test if two linked lists overlaps
     
-    :type x: int
-    :rtype: int
+    :type L1: ListNode
+    :type L2: ListNode
+    :rtype: ListNode[None]
     """
-    #TODO
-    raise NotImplemented
+    def length(L):
+        length = 0
+        while L:
+            length += 1
+            L = L.next
+        return length
+    
+    L1_len, L2_len = length(L1), length(L2)
+    if L1_len > L2_len:
+        L1, L2 = L2, L1 # L2 is the longer list
+    # Advances the longer list to get equal length lists.
+    for _ in range(abs(L1_len - L2_len)):
+        L2 = L2.next
+    while L1 and L2 and L1 is not L2:
+        L1, L2 = L1.next, L2.next
+    return L1 # None implies there is no overlap between L1 and L2.
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(overlapping_no_cycle_lists(L1, L2))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
