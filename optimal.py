da1d95e59582560928423a61a56923f650664eab
veselovich <129292923+veselovich@users.noreply.github.com>
2024-01-10 18:37:52 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,33 @@
 import time
 from memory_profiler import profile
+from nodes import *
 
 
 @profile
-def func_name(x):
+def remove_duplicates(L):
     """
-    Name
-    Example
+    Remove duplicate
     
-    :type x: int
-    :rtype: int
+    :type x: ListNode
+    :rtype: None
     """
-    #TODO
-    raise NotImplemented
+    it = L
+    while it:
+        # Uses next_distinct to find the next distinct value.
+        next_distinct = it.next
+        while next_distinct and next_distinct.data == it.data:
+            next_distinct = next_distinct.next
+        it.next = next_distinct
+        it = next_distinct
+    return L
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    remove_duplicates(L1)
+    print_list(L1)
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
