f4fa1c5f1ca8780f062e46ea489140a6243c3791
veselovich <129292923+veselovich@users.noreply.github.com>
2024-01-10 18:18:15 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,10 @@
 import time
 from memory_profiler import profile
+from nodes import *
 
 
 @profile
-def func_name(x):
+def remove_kth_last(L, k):
     """
     Name
     Example
@@ -11,15 +12,25 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    # Assumes L has at least k nodes, deletes the k-th last node in L.
+    dummy_head = ListNode(0, L)
+    first = dummy_head.next
+    for _ in range(k):
+        first = first.next
+    second = dummy_head
+    while first:
+        first, second = first.next, second.next
+    # second points to the (k + 1)-th last node, deletes its successor.
+    second.next = second.next.next
+    return dummy_head.next
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    remove_kth_last(L1, 2)
+    print_list(L1)
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
