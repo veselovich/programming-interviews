73565b7194440653645710bb2c1f2f3f10c248f2
veselovich <129292923+veselovich@users.noreply.github.com>
2024-01-10 20:55:15 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,10 @@
 import time
 from memory_profiler import profile
+from nodes import *
 
 
 @profile
-def func_name(x):
+def even_odd_merge(L):
     """
     Name
     Example
@@ -11,15 +12,26 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    if not L:
+        return L
+    
+    even_dummy_head, odd_dummy_head = ListNode(0), ListNode(0)
+    tails, turn = [even_dummy_head, odd_dummy_head], 0
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next
+        turn ^= 1 # Alternate between even and odd.
+    tails[1].next = None
+    tails[0].next = odd_dummy_head.next
+    return even_dummy_head.next
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print_list(even_odd_merge(L1))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
