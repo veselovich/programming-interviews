54b5928dbb4b60a49b8f2cd9bf82879c68e11dcb
veselovich <129292923+veselovich@users.noreply.github.com>
2024-01-12 17:54:42 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,11 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
+def add_two_numbers(L1, L2):
     """
     Name
     Example
@@ -11,15 +13,22 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    place_iter = dummy_head = ListNode()
+    carry = 0
+    while L1 or L2 or carry:
+        val = carry + (L1.data if L1 else 0) + (L2.data if L2 else 0)
+        L1 = L1.next if L1 else None
+        L2 = L2.next if L2 else None
+        place_iter.next = ListNode(val % 10)
+        carry, place_iter = val // 10, place_iter.next
+    return dummy_head.next
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print_list(add_two_numbers(L1, L2))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
