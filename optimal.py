7fda40261d91ab915ad06096bcb6a7fc83b2f04e
veselovich <129292923+veselovich@users.noreply.github.com>
2024-01-23 16:07:08 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,7 +3,7 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def evaluate(RPN_expression):	
     """
     Name
     Example
@@ -11,15 +11,27 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    intermediate_results = []
+    DELIMITER = ','
+    OPERATORS = {
+        '+': lambda y, x: x + y, '-': lambda y, x: x - y, '*':
+        lambda y, x: x * y, '/': lambda y, x: int (x / y)
+    }
+    for token in RPN_expression.split(DELIMITER):
+        if token in OPERATORS:
+            intermediate_results.append(OPERATORS[token](
+                intermediate_results.pop(), intermediate_results.pop()))
+        else: # token is a number.
+            intermediate_results.append(int(token))
+    return intermediate_results[-1]
+
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(evaluate("3,4,+,2,*,1,+"))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
