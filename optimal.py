9902a92b2129a6f3bdc806917ef349b3109d38fb
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-02 14:54:44 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,11 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
+def is_symmetric(tree):
     """
     Name
     Example
@@ -11,15 +13,23 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    def check_symmetric(subtree_0, subtree_1):
+        if not subtree_0 and not subtree_1:
+            return True
+        elif subtree_0 and subtree_1:
+            return (subtree_0.data == subtree_1.data
+                    and check_symmetric(subtree_0.left, subtree_1. right)
+                    and check_symmetric(subtree_0.right, subtree_1. left))
+        # One subtree is empty, and the other is not.
+        return False
+    return not tree or check_symmetric(tree.left, tree.right)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(is_symmetric(root_tree))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
