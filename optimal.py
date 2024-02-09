1229d28990c43563e8555cbbaf11f09aeb5b521a
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-08 20:24:25 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,10 @@
 import time
 from memory_profiler import profile
 
+from nodes import tree
 
 @profile
-def func_name(x):
+def find_successor(node):
     """
     Name
     Example
@@ -11,15 +12,26 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    if node.right:
+        # Successor is the leftmost element in node's right subtree.
+        node = node.right
+        while node.left:
+            node = node.left
+        return node
+    
+    # Find the closest ancestor whose left subtree contains node.
+    while node.parent and node.parent.right is node:
+        node = node.parent
+    # A return value of None means node does not have successor, i.e., node is
+    # the rightmost node in the tree.
+    return node.parent
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_successor(tree.root.right.left))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
