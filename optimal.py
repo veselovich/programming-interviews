abf7b9d5427becde7cfbf315c228b8d9901bd7b4
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-11 11:31:46 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,10 @@
 import time
 from memory_profiler import profile
 
+from nodes import TreeNode, print_tree
 
 @profile
-def func_name(x):
+def reconstruct_preorder(preorder):
     """
     Name
     Example
@@ -11,15 +12,23 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    def reconstruct_preorder_helper(preorder_iter):
+        subtree_key = next(preorder_iter)
+        if subtree_key is None:
+            return None
+        # Note that reconstruct-preorder helper updates preorder iter. So the
+        # order of following two calls are critical.
+        left_subtree = reconstruct_preorder_helper(preorder_iter)
+        right_subtree = reconstruct_preorder_helper(preorder_iter)
+        return TreeNode(subtree_key, left_subtree, right_subtree)
+    return reconstruct_preorder_helper(iter(preorder))
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print_tree(reconstruct_preorder(['H','B','F',None,None,'E','A',None,None,None,'C',None,'D',None,'G','I',None,None,None]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
