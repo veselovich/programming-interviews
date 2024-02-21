73451946b90d8567a4daeb30e303d18f7ebd61d4
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-21 16:43:38 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,7 +3,7 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def search_entry_equal_to_its_index(A):
     """
     Name
     Example
@@ -11,15 +11,25 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    left, right = 0, len(A) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        difference = A[mid] - mid
+        # A[mid] == mid if and only if difference == 0.
+        if difference == 0:
+            return mid
+        elif difference > 0:
+            right = mid - 1
+        else: # difference ‹ 0.
+            left = mid + 1
+    return -1
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(search_entry_equal_to_its_index([-2,0,2,4,6,7,9]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
