3f6e223a8e28c3488a80c0a8c1a09de06c3494e6
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-22 20:34:01 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,7 +3,7 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def search_smallest(A):
     """
     Name
     Example
@@ -11,15 +11,24 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    left, right = 0, len(A) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] > A[right]:
+            # Minimum must be in A[mid + 1: right + 1].
+            left = mid + 1
+        else: # A[mid] < A[right].
+            # Minimum cannot be in A[mid + 1:right + 1] so it must be in A[left:mid + 1].
+            right = mid
+    # Loop ends when left == right.
+    return left
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(search_smallest([378,478,550,631,103,203,220,234,279,368]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
