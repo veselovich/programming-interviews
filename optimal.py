8b63d179f319dc087ee71aeab249bfd2d152697f
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-24 12:27:30 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,39 @@
+import collections
 import time
 from memory_profiler import profile
 
 
+MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))
+
 @profile
-def func_name(x):
-    """
-    Name
-    Example
+def find_min_max(A):
+    def min_max(a, b):
+        return MinMax(a, b) if a < b else MinMax(b, a)
     
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+    if len(A) <= 1:
+        return MinMax(A[0], A[0])
+
+    global_min_max = min_max(A[0], A[1])
+    # Process two elements at a time.
+    for i in range(2, len(A) - 1, 2):
+        local_min_max = min_max(A[i], A[i + 1])
+        global_min_max = MinMax(
+            min(global_min_max.smallest, local_min_max.smallest),
+            max(global_min_max.largest, local_min_max.largest))
+    # If there is odd number of elements in the array, we still need to
+    # compare the last element with the existing answer.
+    if len(A) % 2:
+        global_min_max = MinMax(
+            min(global_min_max.smallest, A[-1]),
+            max(global_min_max.largest, A[-1]))
+    return global_min_max
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_min_max([3,2,5,1,2,4]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
