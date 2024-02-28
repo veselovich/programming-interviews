87ee6cd6e1f9a297cef418db046913b7000a7190
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-28 13:33:42 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,9 +1,11 @@
+import itertools
+import random
 import time
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
+def find_missing_element(stream):
     """
     Name
     Example
@@ -11,15 +13,43 @@ def func_name(x):
     :type x: int
     :rtype: int
     """
-    #TODO
-    raise NotImplemented
+    NUM_BUCKET = 1 << 16
+    counter = [0] * NUM_BUCKET
+    stream, stream_copy = itertools.tee(stream)
+    for x in stream:
+        upper_part_x = x >> 16
+        counter[upper_part_x] += 1
+
+    # Look for a bucket that contains less than (1 << 16) elements.
+    BUCKET_CAPACITY = 1 << 16
+    candidate_bucket = next(i for i, c in enumerate(counter)
+                            if c < BUCKET_CAPACITY)
+    
+    # Finds all IP addresses in the stream whose first 16 bits are equal to
+    # candidate_bucket.
+    candidates = [0] * BUCKET_CAPACITY
+    stream = stream_copy
+    for x in stream_copy:
+        upper_part_x = x >> 16
+        if candidate_bucket == upper_part_x:
+            # Records the presence of 16 LSB of x.
+            lower_part_x = ((1 << 16) - 1) & x
+            candidates[lower_part_x] = 1
+
+    # At least one of the LSB combinations is absent, find it.
+    for i, v in enumerate(candidates):
+        if v == 0:
+            return (candidate_bucket << 16) | i
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    all_integers = list(range(0,100))
+    random_ip = [random.randint(100, 2**32) for _ in range(10000)]
+    all_integers += random_ip
+    print(find_missing_element(all_integers))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
