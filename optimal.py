ee4c84113c3db99c5f1debae73e9efb0dc09f88c
veselovich <129292923+veselovich@users.noreply.github.com>
2024-02-29 12:36:55 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,26 @@
+import collections
 import time
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_anagrams(dictionary):
+    sorted_string_to_anagrams = collections.defaultdict(list)
+    for s in dictionary:
+        # Sorts the string, uses it as a key, and then appends the original
+        # string as another value into hash table.
+        sorted_string_to_anagrams[''.join(sorted(s))].append(s)
+    return [
+        group for group in sorted_string_to_anagrams.values() if len(group) >= 2
+    ]
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    dictionary = ['debitcard', 'elvis', 'silent', 'badcredit', 'lives', 'freedom', 'listen', 'levis', 'money']
+    print(find_anagrams(dictionary))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
