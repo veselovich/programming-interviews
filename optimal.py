07eff5641f4fc42560d8a4aab3d9922a5f78ce61
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-03 12:39:36 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,23 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_nearest_repetition(paragraph):
+    word_to_latest_index, nearest_repeated_distance = {}, float('inf')
+    for i, word in enumerate(paragraph):
+        if word in word_to_latest_index:
+            latest_equal_word = word_to_latest_index[word]
+            nearest_repeated_distance = min(nearest_repeated_distance,
+                                            i - latest_equal_word)
+        word_to_latest_index[word] = i
+    return nearest_repeated_distance if nearest_repeated_distance != float(
+        'inf') else -1
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_nearest_repetition("All work and no play makes for no work no fun and no results".split()))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
