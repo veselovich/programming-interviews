23f8a4ea87c8cefd42801ef4abbf1fd1ad2fe7d1
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-08 13:58:49 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,28 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def longest_subarray_with_distinct_entries(A):
+    # Records the most recent occurrences of each entry.
+    most_recent_occurrence = {}
+    longest_dup_free_subarray_start_idx = result = 0
+    for i, a in enumerate(A):
+        # Defer updating dup_id until we see a duplicate.
+        if a in most_recent_occurrence:
+            dup_idx = most_recent_occurrence[a]
+            # A[i] appeared before. Did it appear in the longest current
+            # subarray?
+            if dup_idx >= longest_dup_free_subarray_start_idx:
+                result = max(result, i - longest_dup_free_subarray_start_idx)
+                longest_dup_free_subarray_start_idx = dup_idx + 1
+        most_recent_occurrence[a] = i
+    return max(result, len(A) - longest_dup_free_subarray_start_idx)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(longest_subarray_with_distinct_entries(['e','f','s','f','e','t','w','e','n','w','e']))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
