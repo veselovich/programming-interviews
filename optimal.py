81044e8051829d58eee9f39752b529548a4d5a55
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-08 23:11:38 -0500

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,36 @@
+import collections
 import time
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
+def find_all_substrings(s, words):
+    def match_all_words_in_dict(start):
+        curr_string_to_freq = collections.Counter()
+        for i in range(start, start + len(words) * unit_size, unit_size):
+            curr_word = s[i:i + unit_size]
+            it = word_to_freq[curr_word]
+            if it == 0:
+                return False
+            curr_string_to_freq[curr_word] += 1
+            if curr_string_to_freq[curr_word] > it:
+                # curr_word occurs too many times for a match to be possible.
+                return False
+        return True
     
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+    word_to_freq = collections.Counter(words)
+    unit_size = len(words[0])
+    return [
+        i for i in range(len(s) - unit_size * len(words) + 1)
+        if match_all_words_in_dict(i)
+        ]
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_all_substrings("amanaplanacanal", ['can','apl','ana']))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
