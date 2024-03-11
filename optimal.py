284e38c5bb3391b766619fd6448454a564c60d3e
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-11 17:00:58 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,20 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def smallest_nonconstructible_value(A):
+    max_constructible_value = 0
+    for a in sorted(A):
+        if a > max_constructible_value + 1:
+            break
+        max_constructible_value += a
+    return max_constructible_value + 1
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(smallest_nonconstructible_value([1,1,1,1,1,1,5,10,25]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
