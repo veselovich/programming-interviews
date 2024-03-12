06cc2c52ea6d0a88ad1fc4d7adfc02cebbb3829e
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-11 22:01:25 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,45 @@
+import collections
 import time
 from memory_profiler import profile
 
 
+# Event is a tuple (start_time, end_time)
+Event = collections.namedtuple('Event', ('start', 'finish'))
+
+# Endpoint is a tuple (start_time, 0) or (end_time, 1) so that if times
+# are equal, start_time comes first
+Endpoint = collections.namedtuple('Endpoint', ('time', 'is_start'))
+
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_max_simultaneous_events(A):
+    # Builds an array of all endpoints.
+    E = ([Endpoint(event.start, True)
+          for event in A] + [Endpoint(event.finish, False) for event in A])
+    # Sorts the endpoint array according to the time, breaking ties by putting
+    # start times before end times.
+    E.sort(key=lambda e: (e.time, not e.is_start))
+
+    # Track the number of simultaneous events, record the maximum number of
+    # simultaneous events.
+    max_num_simultaneous_events, num_simultaneous_events = 0, 0
+    for e in E:
+        if e.is_start:
+            num_simultaneous_events += 1
+            max_num_simultaneous_events = max(num_simultaneous_events,
+                                               max_num_simultaneous_events)
+        else:
+            num_simultaneous_events -= 1
+    return max_num_simultaneous_events
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    A = [Event(1,5), Event(6,10), Event(11,13), Event(14,15),
+         Event(2,7), Event(8,9), Event(12,15),
+         Event(4,5), Event(9,17)]
+    print(find_max_simultaneous_events(A))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
