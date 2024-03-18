fe604a311bdbda1c43679367d0efefa48960640f
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-18 18:44:15 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,30 @@
+import collections
+import random
 import time
-from memory_profiler import profile
 
 
-@profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+class Team:
+    Player = collections.namedtuple('Player', ('height'))
+
+    def __init__(self, height):
+        self._players = [Team.Player(h) for h in height]
+
+    # Checks if A can be placed in front of B.
+    @staticmethod
+    def valid_placement_exists(A, B):
+        return all(a < b
+                   for a, b in zip(sorted(A._players), sorted(B._players)))
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    t = [175, 158, 201, 203, 193, 205, 166, 210, 189, 195, 178]
+    team_A = Team(t)
+    team_B = Team([h + 5 for h in t])
+
+    print(Team.valid_placement_exists(team_A, team_B))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
