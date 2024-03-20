b2eb50ddc28042a5c490e09822f2a11faf6d3e6f
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-20 14:27:29 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -3,23 +3,24 @@ from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_salary_cap(target_payroll, current_salaries):
+    current_salaries.sort()
+    unadjusted_salary_sum = 0.0
+    for i, current_salary in enumerate(current_salaries):
+        adjusted_people = len(current_salaries) - i
+        adjusted_salary_sum = current_salary * adjusted_people
+        if unadjusted_salary_sum + adjusted_salary_sum >= target_payroll:
+            return (target_payroll - unadjusted_salary_sum) / adjusted_people
+        unadjusted_salary_sum += current_salary
+    # No solution, since target_payroll > existing payroll.
+    return -1.0
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_salary_cap(210, [90,30,100,40,20]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
