cf9e34580819e07401565747fc4cf33a8d8f29eb
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-21 14:38:32 -0400

    initial commit

---
+++ nodes.py
@@ -0,0 +1,78 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f"Node {self.data}"
+
+
+def print_tree(root):
+    lines, *_ = _display_aux(root)
+    for line in lines:
+        print(line)
+
+
+def _display_aux(root):
+    """Returns list of strings, width, height, and horizontal coordinate of the root."""
+    # No child.
+    if root.right is None and root.left is None:
+        line = '%s' % root.data
+        width = len(line)
+        height = 1
+        middle = width // 2
+        return [line], width, height, middle
+
+    # Only left child.
+    if root.right is None:
+        lines, n, p, x = _display_aux(root.left)
+        s = '%s' % root.data
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+        second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+        shifted_lines = [line + u * ' ' for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+    # Only right child.
+    if root.left is None:
+        lines, n, p, x = _display_aux(root.right)
+        s = '%s' % root.data
+        u = len(s)
+        first_line = s + x * '_' + (n - x) * ' '
+        second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+        shifted_lines = [u * ' ' + line for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+    # Two children.
+    left, n, p, x = _display_aux(root.left)
+    right, m, q, y = _display_aux(root.right)
+    s = '%s' % root.data
+    u = len(s)
+    first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+    second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    if p < q:
+        left += [n * ' '] * (q - p)
+    elif q < p:
+        right += [m * ' '] * (p - q)
+    zipped_lines = zip(left, right)
+    lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+    return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+root_tree = TreeNode(1)
+root_tree.left = TreeNode(2)
+root_tree.right = TreeNode(3)
+root_tree.left.left = TreeNode(4)
+root_tree.left.right = TreeNode(5)
+root_tree.right.left = TreeNode(6)
+root_tree.right.right = TreeNode(7)
+
+root_tree2 = TreeNode(4)
+root_tree2.left = TreeNode(2)
+root_tree2.right = TreeNode(6)
+root_tree2.left.left = TreeNode(1)
+root_tree2.left.right = TreeNode(3)
+root_tree2.right.left = TreeNode(5)
+root_tree2.right.right = TreeNode(7)
--- optimal.py
+++ optimal.py
@@ -1,25 +1,26 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+# Input nodes are nonempty and the key at s is less than or equal to that at b.
+def find_LCA(tree, s, b):
+    while tree.data < s.data or tree.data > b.data:
+        # Keep searching since tree is outside of [s, b].
+        while tree.data < s.data:
+            tree = tree.right # LCA must be in tree's right child.
+        while tree.data > b.data:
+            tree = tree.left # LCA must be in tree's left child.
+    # Now, s.data < tree. data && tree. data <= b. data.
+    return tree
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_LCA(root_tree2, root_tree2.right.left, root_tree2.right.right))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
