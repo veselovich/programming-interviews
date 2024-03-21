67097b3addcc59758d67190f537206b51022d874
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-20 21:15:31 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,25 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_first_greater_than_k(tree, k):
+    subtree, first_so_far = tree, None
+    while subtree:
+        if subtree.data > k:
+            first_so_far, subtree = subtree, subtree.left
+        else: # Root and all keys in left subtree are < k, so skip them.
+            subtree = subtree.right
+    return first_so_far
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_first_greater_than_k(root_tree2, 4.5))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
