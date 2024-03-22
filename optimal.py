e4fefb8fb12385388bc47fe27e94e48ef5936920
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-21 20:58:58 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,36 @@
+import bintrees
 import time
 from memory_profiler import profile
 
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def find_closest_elements_in_sorted_arrays(sorted_arrays):
+    min_distance_so_far = float('inf')
+    # Stores array iterators in each entry.
+    iters = bintrees.RBTree()
+    for idx, sorted_array in enumerate(sorted_arrays):
+        it = iter(sorted_array)
+        first_min = next(it, None)
+        if first_min is not None:
+            iters.insert((first_min, idx), it)
+
+    while True:
+        min_value, min_idx = iters.min_key()
+        max_value = iters.max_key()[0]
+        min_distance_so_far = min(max_value - min_value, min_distance_so_far)
+        it = iters.pop_min()[1]
+        next_min = next(it, None)
+        # Return if some array has no remaining elements.
+        if next_min is None:
+            return min_distance_so_far
+        iters.insert((next_min, min_idx), it)
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(find_closest_elements_in_sorted_arrays([[5,10,15], [3,6,9,12,15], [8,16,24]]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
