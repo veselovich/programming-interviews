638072ba4503b75198e0ef02d5ed2c8a1361d834
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-25 10:55:51 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,37 @@
+import collections
 import time
 from memory_profiler import profile
 
+from nodes import *
+
+Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def range_lookup_in_bst(tree, interval):
+    def range_lookup_in_bst_helper(tree):
+        if tree is None:
+            return
+        
+        if interval.left <= tree.data <= interval.right:
+            # tree. data lies in the interval.
+            range_lookup_in_bst_helper(tree.left)
+            result.append(tree.data)
+            range_lookup_in_bst_helper(tree.right)
+        elif interval.left > tree.data:
+            range_lookup_in_bst_helper(tree.right)
+        else: # interval.right > tree.data
+            range_lookup_in_bst_helper(tree.left)
+
+    result = []
+    range_lookup_in_bst_helper(tree)
+    return result
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print(range_lookup_in_bst(root_tree2, Interval(3,5)))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
