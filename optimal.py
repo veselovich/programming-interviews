fdb14ad0afba3661125344d53632c73e3a74dcaa
veselovich <129292923+veselovich@users.noreply.github.com>
2024-03-24 21:06:29 -0400

    initial commit

--- optimal.py
+++ optimal.py
@@ -1,25 +1,26 @@
 import time
 from memory_profiler import profile
 
+from nodes import *
+
 
 @profile
-def func_name(x):
-    """
-    Name
-    Example
-    
-    :type x: int
-    :rtype: int
-    """
-    #TODO
-    raise NotImplemented
+def build_min_height_bst_from_sorted_array(A):
+    def build_min_height_bst_from_sorted_subarray(start, end):
+        if start >= end:
+            return None
+        mid = (start + end) // 2
+        return TreeNode(A[mid],
+                       build_min_height_bst_from_sorted_subarray(start, mid),
+                       build_min_height_bst_from_sorted_subarray(mid + 1, end))
+    return build_min_height_bst_from_sorted_subarray(0, len(A))
 
 
 def main():
     start_time = time.time()
 
     #test case
-    print(func_name(0))
+    print_tree(build_min_height_bst_from_sorted_array([2,3,5,7,11,13,17,19,23]))
 
     end_time = time.time()
     print(f"\nExecution time: {end_time - start_time:.2}s")
